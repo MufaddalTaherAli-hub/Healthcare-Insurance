c28773f8bec1483c182e2186734e2ab199014493
MufaddalTaherAli-hub <mufaddaltaherali615@gmail.com>
2025-01-12 18:42:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from streamlit_pdf_viewer import pdf_viewer
+from streamlit_option_menu import option_menu
+import time
+
+with st.sidebar:
+    selected=option_menu(
+        menu_title='Menu',
+        options=['Home', 'Data Analysis'],
+        icons=['house', 'bar-chart'],
+        default_index=0
+    )
+
+
+if selected == 'Home':
+    from multiple_pages.predict_page import show_predict_page
+    show_predict_page()
+else:
+    from multiple_pages.Analysis_page import show_analysis_page
+    show_analysis_page()
+
+
+
